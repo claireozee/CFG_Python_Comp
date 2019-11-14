7fcddf2db554c5f6f2b8dc525b4eea0d15731dc2
ozeecodes <47978741+ozeecodes@users.noreply.github.com>
2019-11-14 21:06:58 +0000

    api

--- songs.py
+++ songs.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import requests, os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask("MyExampleApp")
 
@@ -17,32 +19,26 @@ def contact_page():
 	return render_template("contactus.html")
 
 @app.route("/apiexample", methods=["POST"])
-def api_example():
-	#We use the request module to easily collect all the data input into the form
-	form_data = request.form
-	input_movie_name = form_data["movie"]
+def song():
+    #We use the request module to easily collect all the data input into the form
+    form_data = request.form
+    feeling = form_data["feeling"]
+    secondq = form_data["two"]
+    results = birdie()
+    #The second argument of the render_template method lets us send data into our html form
+    #You can pass multiple things in - just separate them with commas
+    #You can also pass in data in lists, and then pull out items from the list within the.html file itself!
+    return render_template("apiexample.html", results=results, feeling=feeling, secondq=secondq)
 
-	results = get_movies(input_movie_name)
-
-	#The second argument of the render_template method lets us send data into our html form
-	#You can pass multiple things in - just separate them with commas
-	#You can also pass in data in lists, and then pull out items from the list within the.html file itself!
-	return render_template("api_example.html", movieresults=results, user_data=form_data)
-
-def get_movies(input_movie_name):
-	load_dotenv();
-	api_key = os.getenv("OMDB_API_KEY")
-	
-	endpoint = 'http://www.omdbapi.com'
-	payload = {"apikey": api_key, "s":input_movie_name}
-	response = requests.get(endpoint, params=payload)
-
-	json_data = response.json()
-
-	#You'll see any printed data in your terminal - helpful to understand what's happening, and to debug
-	print "JSON response from the API call:"
-	print json_data
-
-	return json_data["Search"]
+def birdie():
+    load_dotenv()
+    api_key = os.getenv("SPOTIFY_API_KEY")
+    api_secret = os.getenv("SPOTIFY_API_SECRET")
+    client_credentials_manager = SpotifyClientCredentials(api_key,api_secret)
+    spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+    results = spotify.artist_albums(birdy_uri, album_type='album')
+    print results
+    return results['items'][4]['name']
 
 app.run(debug=True)

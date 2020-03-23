d10dd8459de818f3ce9e8a24ecc7dc8a2308be57
ozeecodes <47978741+ozeecodes@users.noreply.github.com>
2020-03-23 22:14:48 +0000

    minor changes

--- site.py
+++ site.py
@@ -1,3 +1,8 @@
+# 1 - select current mood
+# 2 - based on input, pick what you want to feel / we suggest playlists to change that mood
+# 3 - option to play individual songs
+# 4 - extra: select your own playlist from suggested songs
+
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 import requests, os
@@ -10,6 +15,9 @@ app = Flask("SongSite")
 @app.route("/")
 def home_page():
 	return render_template("index.html")
+
+
+
 
 
 

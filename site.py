aa6f0634b3d2cd6273b28a583c10371c0c3dbd5b
ozeecodes <47978741+ozeecodes@users.noreply.github.com>
2020-03-25 23:54:31 +0000

    site with incorrect api code commented out

--- site.py
+++ site.py
@@ -1,7 +1,9 @@
 # 1 - select current mood
 # 2 - based on input, pick what you want to feel / we suggest playlists to change that mood
-# 3 - option to play individual songs
-# 4 - extra: select your own playlist from suggested songs
+# 3 - results
+# 4 - option to play individual songs
+# 5 - option to select a particular artist or song if not satisfied
+# 6 - extra: create your own playlist from suggested songs
 
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
@@ -12,11 +14,32 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask("SongSite")
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home_page():
-	return render_template("index.html")
+#    form_data = request.form
+#    currentmood = form_data["currentmood"]
+#    wanttofeel = form_data["wanttofeel"]
+#   results = suggested_playlist()
+    return render_template("index.html")#, currentmood=currentmood, wanttofeel=wanttofeel, results=results)
 
+#def suggested_playlist():
+    #"logging in" where else can i put this in terms of entire structure?
+#    load_dotenv()
+#    api_key = os.getenv("SPOTIFY_TOKEN")
+#    api_secret = os.getenv("SPOTIFY_SECRET")
+#    client_credentials_manager = SpotifyClientCredentials(api_key, api_secret)
+#    spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+#    results = spotify.playlist_name_description(name='name', album_type='album', artists='artists')
+#    print results
+#    return results['items'][4]['name']
 
+app.run(debug=True)
+#def
+#endpoint 
+#url = 'https://api.spotify.com/v1/browse/categories/{category_id}/playlists'
+
+#r = requests.get(url)
+#print (r.text)
 
 
 
